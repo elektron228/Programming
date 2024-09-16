7be6c762c5c9f6b8c8e73a5563d0c1ad492cb69b
Владислав <elektronvladklochan@gmail.com>
2024-09-16 23:02:06 +0700

    Start 1 lab

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Item
+    {
+        private readonly int _id;
+        private string _name;
+        private string _info;
+        private float _cost;
+    }
+}
