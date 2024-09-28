252dbec53a91c5fd6a37da989499397581bf4f4b
Владислав <elektronvladklochan@gmail.com>
2024-09-28 08:27:52 +0700

    upd

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Classes/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model.Classes
+{
+    /// <summary>
+    /// Хранит информацию о заказе.
+    /// </summary>
+    public class Order
+    {
+        private static int _idCounter;
+        private int _id;
+        private float _totalPrice;
+        private Address _deliveryAddress;
+        private DateTime _creationDate;
+    }
+}
