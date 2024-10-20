80e35f11c270a867c3c85a86188cc1162637516c
Владислав <elektronvladklochan@gmail.com>
2024-10-20 19:54:52 +0700

    Complete discount classes

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Classes/Discounts/PercentDiscount.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model.Classes.Discounts
+{
+    public class PercentDiscount
+    {
+        private int _percent = 1;
+        private Category _category;
+        private double _totalSum;
+
+        /// <summary>
+        /// Возвращает размер скидки в процентах.
+        /// </summary>
+        public int Percent
+        {
+            get { return _percent; }
+            private set { _percent = value; }
+        }
+
+        /// <summary>
+        /// Возвращает категорию скидки.
+        /// </summary>
+        public Category Category { get { return _category; } }
+
+        /// <summary>
+        /// Возвращает информацию о скидке.
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                return $"Процентная '{_category}' - {_percent}%";
+            }
+        }
+
+        /// <summary>
+        /// Возвращает размер доступной скидки.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public double Calculate(List<Item> items)
+        {
+            int percent = _percent;
+            double sum = 0;
+            foreach (var item in items)
+            {
+                if (item.Category == _category)
+                {
+                    sum += item.Cost;
+                }
+            }
+            double sum2 = sum;
+            while((sum2 >= 1000) && (percent < 10))
+            {
+                sum -= 1000;
+                percent++;
+            }
+            return (sum / 100) * percent;
+        }
+
+        /// <summary>
+        /// Применяет скидку к товарам.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public double Apply(List<Item> items)
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                if (item.Category == _category)
+                {
+                    sum += item.Cost;
+                }
+            }
+            double sum2 = sum;
+            while ((sum2 >= 1000) && (_percent < 10))
+            {
+                sum -= 1000;
+                _percent++;
+            }
+            return (sum / 100) * _percent;
+
+        }
+
+        /// <summary>
+        /// Обновляет итоговую сумму покупок.
+        /// </summary>
+        /// <param name="items"></param>
+        public void Update(List<Item> items)
+        {
+            foreach (var item in items)
+            {
+                if (item.Category == _category)
+                {
+                    _totalSum += item.Cost;
+                }
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Classes/Discounts/PointsDiscount.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model.Classes
+{
+    /// <summary>
+    /// Хранит данные о накопительной скидке.
+    /// </summary>
+    public class PointsDiscount
+    {
+        private int _points = 0;
+
+        /// <summary>
+        /// Возвращает количество накопленных баллов.
+        /// </summary>
+        public int Points 
+        {
+            get { return _points; }
+            private set 
+            { 
+                if(value >= 0)
+                {
+                    _points = value;
+
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Возвращает информацию о скидке.
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                if (_points % 100 >= 11 && _points % 100 <= 14)
+                {
+                    return $"Накопительная - {_points} баллов";
+                }
+
+                int lastDigit = _points % 10;
+
+                if (lastDigit == 1 && _points % 100 != 11)
+                {
+                    return $"Накопительная - {_points} балл";
+                }
+                else if (lastDigit >= 2 && lastDigit <= 4 && _points % 100 != 12 && _points % 100 != 13 && _points % 100 != 14)
+                {
+                    return $"Накопительная - {_points} балла";
+                }
+                else
+                {
+                    return $"Накопительная - {_points} баллов";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает размер доступной скидки.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public double Calculate(List<Item> items)
+        {
+            double sum = 0;
+            foreach( var item in items)
+            {
+                sum += item.Cost;
+            }
+
+            double discount = (sum / 100) * 30;
+
+            if (discount > _points)
+            {
+                return _points;
+            }
+            else
+            {
+                return discount;
+            }
+        }
+
+        /// <summary>
+        /// Применяет скидку к товарам. Списывает баллы.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public double Apply(List<Item> items)
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum += item.Cost;
+            }
+
+            double discount = (sum / 100) * 30;
+
+            if (discount > _points)
+            {   
+                discount = _points;
+                _points = 0;
+                return discount;
+            }
+            else
+            {
+                _points  -= Convert.ToInt32(discount);
+                return discount;
+            }
+
+        }
+
+        /// <summary>
+        /// Начисляет баллы после покупки.
+        /// </summary>
+        /// <param name="items"></param>
+        public void Update(List<Item> items)
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum += item.Cost;
+            }
+            double points = (sum / 100) * 10;
+
+            _points += Convert.ToInt32(points);
+        }
+    }
+}
