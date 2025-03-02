c079f32ef64ee598a0c958229b2adc547504ed7c
Владислав <elektronvladklochan@gmail.com>
2025-03-02 21:18:48 +0700

    add Contact.cs class

---
+++ Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    internal class Contact
+    {
+        private string _name;
+        private string _email;
+        private string _phone;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        public string Phone { get { return _phone; } set { _phone = value; } }
+
+        public Contact(string name, string email, string phone) 
+        {
+            Name = name;
+            Email = email;  
+            Phone = phone;  
+        }
+    }
+}
