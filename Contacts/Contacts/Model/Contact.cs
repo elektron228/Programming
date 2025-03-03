1d6430108a700b33c8a4acf612588b8c22102ebd
Владислав <elektronvladklochan@gmail.com>
2025-03-03 22:19:33 +0700

    Add MainVM class

--- Contacts/Contacts/Model/Contact.cs
+++ Contacts/Contacts/Model/Contact.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 namespace View.Model
 {
-    internal class Contact
+    public class Contact
     {
         private string _name;
         private string _email;
         private string _phone;
 
+        /// <summary>
+        /// Возвращает и задаёт имя контакта.
+        /// </summary>
         public string Name { get { return _name; } set { _name = value; } }
+
+        /// <summary>
+        /// Возвращает и задаёт адрес электронной почты.
+        /// </summary>
         public string Email { get { return _email; } set { _email = value; } }
+
+        /// <summary>
+        /// Возвращает и задаёт Номер телефона.
+        /// </summary>
         public string Phone { get { return _phone; } set { _phone = value; } }
 
+        /// <summary>
+        /// Создаёт экзкмпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="email">Адрес электронной почты контакта.</param>
+        /// <param name="phone">Номер телефона контакта.</param>
         public Contact(string name, string email, string phone) 
         {
             Name = name;
---
+++ Contacts/Contacts/ViewModel/MainVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model;
+
+namespace View.ViewModel
+{
+    class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Contact CurrentContact;
+
+        /// <summary>
+        /// Возвращает и задаёт имя контакта.
+        /// </summary>
+        public string Name 
+        { 
+            get 
+            { 
+                return CurrentContact.Name; 
+            } 
+            set 
+            {
+                CurrentContact.Name = value; 
+            } 
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт адрес электронной почты.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return CurrentContact.Email;
+            }
+            set
+            {
+                CurrentContact.Email = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт Номер телефона.
+        /// </summary>
+        public string Phone
+        {
+            get
+            {
+                return CurrentContact.Phone;
+            }
+            set
+            {
+                CurrentContact.Phone = value;
+            }
+        }
+    }
+}
