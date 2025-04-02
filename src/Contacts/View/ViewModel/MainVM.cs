a0412cc53541203f5a2ab0d5f2d6cb9afcce650f
Владислав <elektronvladklochan@gmail.com>
2025-04-02 13:01:12 +0700

    feature: класс MainVM изменён для работы с коллекцией объектов

--- src/Contacts/View/ViewModel/MainVM.cs
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using View.Model;
 using View.Model.Services;
 
@@ -16,14 +18,35 @@ namespace View.ViewModel
         private Contact _currentContact;
 
         /// <summary>
+        /// Копия оригинального контакта.
+        /// </summary>
+        private Contact _originalContact;
+
+        /// <summary>
         /// Класс, выполняющий сериализацию и дисериализацию.
         /// </summary>
         private ContactSerializer _serializer;
 
         /// <summary>
+        /// Показывает, реадктируется ли сйечас контакт.
+        /// </summary>
+        private bool _isEdit = false;
+        
+        /// <summary>
+        /// Показывает, добавляется ли сйечас контакт.
+        /// </summary>
+        private bool _isCreate = false;
+
+        /// <summary>
         /// Оповещает систему об изменении свойства.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Коллекция контактов.
+        /// </summary>
+        public ObservableCollection<Contact> Contacts { get; set; }
+            = new ObservableCollection<Contact>();
 
         /// <summary>
         /// Возвращает и задаёт текущий контакт.
@@ -35,24 +58,34 @@ namespace View.ViewModel
                 return _currentContact; 
             }
             set
-            { 
-                _currentContact = value;
-                OnPropertyChanged(nameof(CurrentContact));
-                OnPropertyChanged(nameof(Name));
-                OnPropertyChanged(nameof(Email));
-                OnPropertyChanged(nameof(Phone));
+            {
+                if (_currentContact != value)
+                {
+                    // Сбрасываем изменения, если они не были применены
+                    if (_isEdit || _isCreate)
+                    {
+                        Name = _originalContact?.Name ?? string.Empty;
+                        Email = _originalContact?.Email ?? string.Empty;
+                        Phone = _originalContact?.Phone ?? string.Empty;
+
+                        _isCreate = false;
+                        _isEdit = false;
+                        OnPropertyChanged(nameof(IsEditing));
+                        OnPropertyChanged(nameof(IsApplyVisible));
+                        OnPropertyChanged(nameof(IsApplyEnabled));
+
+                    }
+
+                    _currentContact = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Name));
+                    OnPropertyChanged(nameof(Email));
+                    OnPropertyChanged(nameof(Phone));
+                }
             }
         }
 
-        /// <summary>
-        /// Выполняет загрузку данных из файл.
-        /// </summary>
-        public LoadCommand LoadCommand { get; }
-
-        /// <summary>
-        /// Выполняет сохранение данных в файл.
-        /// </summary>
-        public SaveCommand SaveCommand {  get; }
+        public Contact NewContact { get; set; }
 
         /// <summary>
         /// Возвращает и задаёт имя контакта.
@@ -67,6 +100,7 @@ namespace View.ViewModel
             {
                 _currentContact.Name = value; 
                 OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(IsApplyEnabled));
             } 
         }
 
@@ -83,6 +117,7 @@ namespace View.ViewModel
             {
                 _currentContact.Email = value;
                 OnPropertyChanged(nameof(Email));
+                OnPropertyChanged(nameof(IsApplyEnabled));
             }
         }
 
@@ -99,19 +134,212 @@ namespace View.ViewModel
             {
                 _currentContact.Phone = value;
                 OnPropertyChanged(nameof(Phone));
+                OnPropertyChanged(nameof(IsApplyEnabled));
             }
         }
+
+        /// <summary>
+        /// Определяет, редактируется ли контакт.
+        /// </summary>
+        public bool IsEditing
+        {
+            get { return _isCreate || _isEdit; }
+        }
+
+        /// <summary>
+        /// Команда для добавления контакта.
+        /// </summary>
+        public ICommand AddCommand { get; private set; }
+
+        /// <summary>
+        /// Команда для редактирования контакта.
+        /// </summary>
+        public ICommand EditCommand { get; private set; }
+
+        /// <summary>
+        /// Команда для удаления контакта.
+        /// </summary>
+        public ICommand RemoveCommand { get; private set; }
+
+        /// <summary>
+        /// Команда для сохранения изменений.
+        /// </summary>
+        public ICommand ApplyCommand { get; private set; }
+
+        /// <summary>
+        /// Показывет, возможно ли добавить новый контакт.
+        /// </summary>
+        public bool IsAddEnabled => !_isEdit && !_isCreate;
+
+        /// <summary>
+        /// Показывет, возможно ли редактировать контакт.
+        /// </summary>
+        public bool IsEditEnabled => CurrentContact != null && !_isEdit && !_isCreate;
+
+        /// <summary>
+        /// Показывет, возможно ли удалить контакт.
+        /// </summary>
+        public bool IsRemoveEnabled => CurrentContact != null && !_isEdit && !_isCreate;
+
+        /// <summary>
+        /// Показывет, возможно ли применить изменения.
+        /// </summary>
+        public bool IsApplyEnabled => (_isCreate && (!string.IsNullOrEmpty(Name) &&
+            !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Phone))) || 
+            (CurrentContact != null && _isEdit &&(!string.IsNullOrEmpty(Name) 
+            && !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Phone))); 
+
+        public bool IsApplyVisible => _isEdit || _isCreate;
 
         /// <summary>
         /// Создаёт экземпляр класса <see cref="MainVM"/>.
         /// </summary>
         public MainVM()
         {
-            _currentContact = new Contact("Иванов Иван Иванович", "+79235678909", "ivanov1999@gmail.com");
-
             _serializer = new ContactSerializer();
-            LoadCommand = new LoadCommand(this._serializer, this);
-            SaveCommand = new SaveCommand(this._serializer, this._currentContact);
+            Contacts = _serializer.LoadContacts();
+
+            AddCommand = new RelayCommand(OnAddCommandExecute, OnAddCommandCanExecute);
+            EditCommand = new RelayCommand(OnEditCommandExecute, OnEditCommandCanExecute);
+            RemoveCommand = new RelayCommand(OnRemoveCommandExecute, OnRemoveCommandCanExecute);
+            ApplyCommand = new RelayCommand(OnApplyCommandExecute, OnApplyCommandCanExecute);
+        }
+
+        /// <summary>
+        /// Выполняет логику добавления нового контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnAddCommandExecute(object obj)
+        {
+            CurrentContact = null;
+            CurrentContact = new Contact();
+            _originalContact = new Contact 
+            {
+                Name = CurrentContact.Name,
+                Email = CurrentContact.Email,
+                Phone = CurrentContact.Phone
+            };
+
+            Name = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+            _isCreate = true;
+
+            OnPropertyChanged(nameof(IsApplyVisible));
+            OnPropertyChanged(nameof(IsApplyEnabled));
+            OnPropertyChanged(nameof(IsEditing));
+        }
+
+        /// <summary>
+        /// Определяет, может ли быть выполнена команда добавления контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool OnAddCommandCanExecute(object obj)
+        {
+            return IsAddEnabled;
+        }
+
+        /// <summary>
+        /// Выполняет логику редактирования существующего контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnEditCommandExecute(object obj)
+        {
+
+            _isEdit = true;
+            _originalContact = new Contact
+            {
+                Name = CurrentContact.Name,
+                Email = CurrentContact.Email,
+                Phone = CurrentContact.Phone
+            };
+            OnPropertyChanged(nameof(IsApplyVisible));
+            OnPropertyChanged(nameof(IsApplyEnabled));
+            OnPropertyChanged(nameof(IsEditing));
+        }
+
+        /// <summary>
+        /// Определяет, может ли быть выполнена команда редактирования контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool OnEditCommandCanExecute(object obj)
+        {
+            return IsEditEnabled;
+        }
+
+        /// <summary>
+        /// Выполняет логику удаления выбранного контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnRemoveCommandExecute(object obj)
+        {
+            if (CurrentContact != null)
+            {
+                int index = Contacts.IndexOf(CurrentContact);
+                Contacts.Remove(CurrentContact);
+                _originalContact = null;
+                _serializer.SaveContacts(Contacts);
+                if (Contacts.Count > 0) 
+                {
+                    if (index < Contacts.Count) 
+                    {
+                        CurrentContact = Contacts[index];
+                    }
+                    else 
+                    {
+                        CurrentContact = Contacts.LastOrDefault(); 
+                    }
+                }
+                else 
+                {
+                    CurrentContact = null; 
+                }
+            }
+
+            OnPropertyChanged(nameof(IsApplyEnabled));
+            OnPropertyChanged(nameof(IsEditing));
+        }
+
+        /// <summary>
+        /// Определяет, может ли быть выполнена команда удаления контакта.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool OnRemoveCommandCanExecute(object obj)
+        {
+            return IsRemoveEnabled;
+        }
+
+        /// <summary>
+        /// Выполняет логику применения изменений к контакту.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnApplyCommandExecute(object obj)
+        {
+            if (_isCreate)
+            {
+                Contacts.Add(CurrentContact);
+                _isCreate = false;
+            }
+            _isEdit = false;
+            _serializer.SaveContacts(Contacts);
+            
+            _originalContact = null;
+            OnPropertyChanged(nameof(IsApplyVisible));
+            OnPropertyChanged(nameof(IsApplyEnabled));
+            OnPropertyChanged(nameof(IsEditing));
+        }
+
+        /// <summary>
+        /// Определяет, может ли быть выполнена команда применения изменений.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private bool OnApplyCommandCanExecute(object obj)
+        {
+            return IsApplyEnabled;
         }
 
         /// <summary>
